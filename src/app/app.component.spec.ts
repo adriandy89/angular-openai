e0e22702faed01718778a6b76e57f8c47d39b203
Adrian DY <adrian.dy89@gmail.com>
2025-01-19 22:40:24 +0100

    create base app structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-openai' title`, () => {
+  it(`should have the 'angularGpt' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-openai');
+    expect(app.title).toEqual('angularGpt');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-openai');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, angularGpt'
+    );
   });
 });
